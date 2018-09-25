6fcb33c1a36241aa20c5eab312367cc97d6ddb2b
developer <jeesoo2002@gmail.com>
2018-09-25 14:30:48 -0700

    finished countdown clock

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,16 @@
 let countdown;
 const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
+  // clear any existing timers
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -30,3 +36,27 @@ function displayTimeLeft(seconds) {
 
   console.log({ minutes, remainderSeconds });
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back @ ${hour > 12 ? hour - 12 : hour}:${
+    minutes < 10 ? "0" : ""
+  }${minutes} ${hour > 12 ? "pm" : "am"}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener("click", startTimer));
+
+document.customForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  timer(mins * 60);
+  console.log(mins);
+  this.reset();
+});
